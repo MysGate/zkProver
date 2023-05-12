8ba57d3a914fa54e87d66069a107e97882edd317
FleetingTimePaul <83365885@qq.com>
2023-05-12 10:45:22 +0800

    Update

--- server.js
+++ server.js
@@ -5,29 +5,13 @@ import bodyParser from 'body-parser'
 const app = express()
 app.use(bodyParser.json())
 
-
-app.get('/hello', (request, response) => {
-  // localhost:2000/hello?a=123&b=456
-  const query = request.query
-  console.log(query)  // { a: '123', c: '456' }
-
-  let data = {
-    foo: 111,
-    bar: 222,
-  }
-  data = JSON.stringify(data)
-  response.send(data)
-})
-
-
-app.post('/world', (request, response) => {
+app.post('/proof', (request, response) => {
   const data = request.body
   console.log('post data', data)
 
-  response.send('123')
+  response.send({proof: "123"})
 })
 
-
 const server = app.listen(2000, () => {
-  console.log(`visit: http://localhost:2000/hello`)
+  console.log(`visit: http://localhost:2000/proof`)
 })
